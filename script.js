8a48209ec23a38351dcde94cfa2ec73c3471f229
Sumant9761 <142734516+Sumant9761@users.noreply.github.com>
2026-06-12 10:30:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const btn = document.getElementById("btn");
+const textInput = document.getElementById("text");
+const delayInput = document.getElementById("delay");
+const output = document.getElementById("output");
+
+function wait(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+}
+
+btn.addEventListener("click", async () => {
+  const text = textInput.value;
+  const delay = Number(delayInput.value);
+
+  output.textContent = "";
+
+  await wait(delay);
+
+  output.textContent = text;
+});
